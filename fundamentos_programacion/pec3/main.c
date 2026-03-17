3e1d90158f04b5b94b998a102082ea855ff79f50
PabloMonteiroX <pablo_monteiro@hotmail.com>
2026-03-17 17:08:34 +0100

    resuelve conflictos PEC3

--- fundamentos_programacion/pec3/main.c
+++ fundamentos_programacion/pec3/main.c
@@ -15,83 +15,85 @@ matrix, and processes the information to display the results. */
 
 #include <stdio.h>
 
-#define MAX_LEN (15 + 1)
+#define MAX_LEN 16
 #define ROWS 3
 #define COLS 3
 #define STOCK_OUT 0
 #define ZONE_WARNING 15
 #define LOW_INVENTORY 100
 
-typedef enum {TSTOCK_OUT, TZONE_WARNING, TLOW_INVENTORY, TOPERATIONAL} tState;
+typedef enum {OPERATIONAL, OUT, WARNING, LOW,} tState;
 
 int main(void) {
 /* ------------------------- Variable declarations -------------------------- */
     tState state;
     FILE   *file;
-    char   filename[MAX_LEN];
-    int    stock[ROWS][COLS];
-    int    zone0Total;
-    int    zone1Total;
-    int    zone2Total;
+    char   fname[MAX_LEN];
+    int    stk[ROWS][COLS];
+    int    row0;
+    int    row1;
+    int    row2;
     int    total;
 /* ----------------------------- Input section ------------------------------ */
     printf("INPUT\n");
     printf("FILENAME?\n");
-    scanf("%15s", filename);
-
-    file = fopen(filename, "r");
-    fscanf(file, "%d %d %d", &stock[0][0], &stock[0][1], &stock[0][2]);
-    fscanf(file, "%d %d %d", &stock[1][0], &stock[1][1], &stock[1][2]);
-    fscanf(file, "%d %d %d", &stock[2][0], &stock[2][1], &stock[2][2]);
+    scanf("%15s", fname);
+    file = fopen(fname, "r");
+    fscanf(file, "%d %d %d", &stk[0][0], &stk[0][1], &stk[0][2]);
+    fscanf(file, "%d %d %d", &stk[1][0], &stk[1][1], &stk[1][2]);
+    fscanf(file, "%d %d %d", &stk[2][0], &stk[2][1], &stk[2][2]);
     fclose(file);
 /* --------------------------- Processing section --------------------------- */
-    /* calculate total rows */
-    zone0Total = stock[0][0] + stock[0][1] + stock[0][2];
-    zone1Total = stock[1][0] + stock[1][1] + stock[1][2];
-    zone2Total = stock[2][0] + stock[2][1] + stock[2][2];
-    /* calculate total matrix */
-    total = zone0Total + zone1Total + zone2Total;
+    row0  = 0;
+    row1  = 0;
+    row2  = 0;
+    total = 0;
+    state = OPERATIONAL;
+    row0  = stk[0][0] + stk[0][1] + stk[0][2];
+    row1  = stk[1][0] + stk[1][1] + stk[1][2];
+    row2  = stk[2][0] + stk[2][1] + stk[2][2];
+    total = row0 + row1 + row2;
 
-    if (stock[0][0] == STOCK_OUT ||
-        stock[0][1] == STOCK_OUT ||
-        stock[0][2] == STOCK_OUT ||
-        stock[1][0] == STOCK_OUT ||
-        stock[1][1] == STOCK_OUT ||
-        stock[1][2] == STOCK_OUT ||
-        stock[2][0] == STOCK_OUT ||
-        stock[2][1] == STOCK_OUT ||
-        stock[2][2] == STOCK_OUT) {
+    if (stk[0][0] == STOCK_OUT ||
+        stk[0][1] == STOCK_OUT ||
+        stk[0][2] == STOCK_OUT ||
+        stk[1][0] == STOCK_OUT ||
+        stk[1][1] == STOCK_OUT ||
+        stk[1][2] == STOCK_OUT ||
+        stk[2][0] == STOCK_OUT ||
+        stk[2][1] == STOCK_OUT ||
+        stk[2][2] == STOCK_OUT) {
 
-        state = TSTOCK_OUT;
+        state = OUT;
 
-    } else if (zone0Total < ZONE_WARNING ||
-               zone1Total < ZONE_WARNING ||
-               zone2Total < ZONE_WARNING) {
+    } else if (row0 < ZONE_WARNING ||
+               row1 < ZONE_WARNING ||
+               row2 < ZONE_WARNING) {
 
-        state = TZONE_WARNING;
+        state = WARNING;
 
     } else if (total < LOW_INVENTORY) {
 
-        state = TLOW_INVENTORY;
+        state = LOW;
 
     } else {
 
-        state = TOPERATIONAL;
+        state = OPERATIONAL;
     }
 /* ----------------------------- Output section ----------------------------- */
     printf("OUTPUT\n");
 
     switch (state) {
 
-        case TSTOCK_OUT:
+        case OUT:
             printf("STOCK OUT\n");
             break;
 
-        case TZONE_WARNING:
+        case WARNING:
             printf("ZONE WARNING\n");
             break;
 
-        case TLOW_INVENTORY:
+        case LOW:
             printf("LOW INVENTORY\n");
             break;
 
